bc203222e58981bebd2cd11aa5b77b3c2acc5a49
bamshoot <jeb.williams8@gmail.com>
2025-01-08 16:40:20 +1000

    begin implementation of rsi indicator

--- server/data_foundation_builder.py
+++ server/data_foundation_builder.py
@@ -930,7 +930,26 @@ class RSIRankBuilder:
         self.timeframe = None
         self.source_table_name = None
         self.table_name = (f"tbl_{self.data_source}_RSIRank")
-        self._reset_ranks_table()
+        self.currency_rsi_values = {
+            'AUD': {'1h': [], 'd': [], 'w': [], 'm': []},
+            'NZD': {'1h': [], 'd': [], 'w': [], 'm': []},
+            'CAD': {'1h': [], 'd': [], 'w': [], 'm': []},
+            'CHF': {'1h': [], 'd': [], 'w': [], 'm': []},
+            'JPY': {'1h': [], 'd': [], 'w': [], 'm': []},
+            'GBP': {'1h': [], 'd': [], 'w': [], 'm': []},
+            'EUR': {'1h': [], 'd': [], 'w': [], 'm': []},
+            'USD': {'1h': [], 'd': [], 'w': [], 'm': []}
+        }
+        self.currency_rsi_avg = {
+            'AUD': {'1h': None, 'd': None, 'w': None, 'm': None},
+            'NZD': {'1h': None, 'd': None, 'w': None, 'm': None},
+            'CAD': {'1h': None, 'd': None, 'w': None, 'm': None},
+            'CHF': {'1h': None, 'd': None, 'w': None, 'm': None},
+            'JPY': {'1h': None, 'd': None, 'w': None, 'm': None},
+            'GBP': {'1h': None, 'd': None, 'w': None, 'm': None},
+            'EUR': {'1h': None, 'd': None, 'w': None, 'm': None},
+            'USD': {'1h': None, 'd': None, 'w': None, 'm': None}
+        }
 
     def __del__(self):
         if hasattr(self, 'con'):
@@ -939,9 +958,111 @@ class RSIRankBuilder:
     def _get_table_from_db(self, table_name: str):
         return self.con.sql(f"SELECT * FROM {table_name}")
 
-    def _reset_ranks_table(self):
+    def reset_rsi_ranks_table(self):
         self._drop_table(self.table_name)
-        self._create_ranks_table()
+        self._create_rsi_ranks_table()
+
+    def _drop_table(self, table_name: str):
+        self.con.sql(f"DROP TABLE IF EXISTS {table_name}")
+
+    def set_instrument_name(self, instrument_name: str):
+        self.instrument_name = instrument_name
+
+    def set_timeframe(self, timeframe: str):
+        self.timeframe = timeframe
+
+    def set_source_table_name(self):
+        self.source_table_name = (f"tbl_{self.data_source}_"
+                                  f"{self.instrument_name}_"
+                                  f"{self.timeframe}")
+
+    def _create_rsi_ranks_table(self):
+        self.con.sql(f"""
+            CREATE TABLE {self.table_name}
+            (
+                instrument_name VARCHAR,
+                base_currency VARCHAR,
+                quote_currency VARCHAR,
+                timeframe VARCHAR,
+                lst_price_datetime DATETIME,
+                pair_rsi DOUBLE,
+                currency_rsi DOUBLE,
+                pair_timeframe_rank INTEGER,
+                currency_timeframe_rank INTEGER
+            )
+        """)
+
+    def generate_rsi(self):
+        data = self._get_table_from_db(self.source_table_name).fetchnumpy()
+        rsi = ta.RSI(data["close"], 14)
+        lst_price_datetime = data["datetime"][-1]
+
+        base_currency = self.instrument_name[:3]
+        quote_currency = self.instrument_name[3:]
+
+        if base_currency in self.currency_rsi_values:
+            if self.timeframe in self.currency_rsi_values[base_currency]:
+                self.currency_rsi_values[base_currency][self.timeframe].append(rsi[-1])
+
+        if quote_currency in self.currency_rsi_values:
+            if self.timeframe in self.currency_rsi_values[quote_currency]:
+                self.currency_rsi_values[quote_currency][self.timeframe].append(rsi[-1])
+
+        self.con.sql(f"""
+            INSERT INTO {self.table_name}
+            (instrument_name, base_currency, quote_currency, timeframe,
+             lst_price_datetime, pair_rsi)
+            VALUES
+            ('{self.instrument_name}', '{base_currency}', '{quote_currency}',
+             '{self.timeframe}', '{lst_price_datetime}', {rsi[-1]})
+        """)
+
+    def generate_pair_timeframe_rank(self):
+        self.con.sql(f"""
+            UPDATE {self.table_name} t1
+            SET pair_timeframe_rank = (
+                SELECT rank
+                FROM (
+                    SELECT
+                        instrument_name,
+                        timeframe,
+                        DENSE_RANK() OVER (
+                            PARTITION BY timeframe
+                            ORDER BY pair_rsi DESC
+                        ) as rank
+                    FROM {self.table_name}
+                ) rankings
+                WHERE rankings.instrument_name = t1.instrument_name
+                AND rankings.timeframe = t1.timeframe
+            )
+        """)
+
+    def generate_currency_rsi(self):
+
+        for currency in self.currency_rsi_values:
+            for timeframe in self.currency_rsi_values[currency]:
+                try:
+                    self.currency_rsi_avg[currency][timeframe] = (
+                        sum(self.currency_rsi_values[currency][timeframe]) /
+                        len(self.currency_rsi_values[currency][timeframe]))
+                    self.con.sql(f"""
+                        UPDATE {self.table_name}
+                        SET currency_rsi = {self.currency_rsi_avg[currency][timeframe]}
+                        WHERE instrument_name LIKE '%{currency}%'
+                        AND timeframe = '{timeframe}'
+                    """)
+                except ZeroDivisionError:
+                    self.currency_rsi_avg[currency][timeframe] = None
+
+    def get_rsi_table(self):
+        return self.con.sql(f"""
+            SELECT * FROM {self.table_name}
+        """)
+
+    def to_csv(self, table_name: str):
+        self.con.sql(f"""
+            SELECT * FROM {table_name} ORDER BY instrument_name, timeframe
+        """).write_csv(f"./outputs/rsi/{table_name}.csv")
 
 
 params = {
@@ -1060,6 +1181,25 @@ start_time = time.time()
 
 # # Replace the loop with:
 # asyncio.run(process_all_instruments())
+
+print("Building RSI ranks")
+
+rsi_rank_builder = RSIRankBuilder("./database/clarity.db", "EOD")
+rsi_rank_builder.reset_rsi_ranks_table()
+
+for instrument in params["instruments"]:
+    for timeframe in params["timeframes"]:
+        print(f"Building {instrument} {timeframe}")
+        rsi_rank_builder.set_instrument_name(instrument)
+        rsi_rank_builder.set_timeframe(timeframe)
+        rsi_rank_builder.set_source_table_name()
+        rsi_rank_builder.generate_rsi()
+
+rsi_rank_builder.generate_pair_timeframe_rank()
+rsi_rank_builder.generate_currency_rsi()
+
+if params["to_csv"]:
+    rsi_rank_builder.to_csv(rsi_rank_builder.table_name)
 
 end_time = time.time()
 print(f"Time taken: {end_time - start_time} seconds")
